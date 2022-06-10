3cf1b3ebf5b1581911287ab0fd6d64e6d093af53
SlothSan <mikeoram01@gmail.com>
2022-06-10 10:55:03 +0100

    Username check logic complete.

--- js-logical-operators/js-login-prompt/javascript.js
+++ js-logical-operators/js-login-prompt/javascript.js
@@ -3,11 +3,14 @@ let password;
 
 userName = prompt("Please Enter Your UserName For The System.");
 
-if (userName == "Admin") {
+if (userName === "Admin") {
     alert("Welcome Admin");
+ 
     
-} else if (userName != "Admin") {
-    alert("User not recognised.");
-} else (userName == null) {
-    alert("Login cancelled");
+
+
+} else if (userName === '' || userName === null) {
+    alert("Login Cancelled.");
+} else {
+    alert("UserName not recognised by the system.");
 }
