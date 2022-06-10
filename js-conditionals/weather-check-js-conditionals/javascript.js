1bed498c1f8ca9d0a93f85f83a32d6d37a95b7cc
SlothSan <mikeoram01@gmail.com>
2022-06-10 12:01:31 +0100

    further renaming and adding switch function to the weather-check.

--- js-conditionals/weather-check-js-conditionals/javascript.js
+++ js-conditionals/weather-check-js-conditionals/javascript.js
@@ -1,7 +1,7 @@
 const select = document.querySelector('select');
 const para = document.querySelector('p');
 
-select.addEventListener('change', setWeather);
+select.addEventListener('change', setWeather2);
 
 function setWeather () {
     const choice = select.value;
@@ -18,5 +18,37 @@ if (choice === 'sunny') {
 } else {
     para.textContent = '';
 }
+
+}
+
+//Rewritten using switches instead of if else if else. 
+
+function setWeather2 () {
+    const choice = select.value;
+
+
+switch(choice) {
+    
+    case 'sunny':
+        para.textContent = 'It is nice and sunny outside today. Wear Shorts! Go to the beach, or the park, and get an ice cream. :)';
+        break;
+
+    case 'rainy':
+        para.textContent = 'Rain is falling outside, take a rain coat and an umbrella with you, and don\'t stay out for too long';
+        break;
+
+    case 'snowing':
+        para.textContent = 'The snow is coming down! It is freezing! Best to stay in with a cup of hot chocolate, or if your feeling brave build a snowman!';
+        break;
+
+    case 'overcast':
+        para.textContent = 'It isn\'t raining but the sky is grey and gloomy, you are probably in the UK and it could turn any minute, best take the big coat just in case.';
+        break;
+
+    default:
+    para.textContent = '';
+
+    
+    }
 
 }
