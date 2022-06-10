868ed61b0ea2ceeecdceae0efc6b2ebbea9a8cc2
SlothSan <mikeoram01@gmail.com>
2022-06-10 11:03:58 +0100

    Login prompt complete - added some html from the JS.

--- js-logical-operators/js-login-prompt/javascript.js
+++ js-logical-operators/js-login-prompt/javascript.js
@@ -4,10 +4,20 @@ let password;
 userName = prompt("Please Enter Your UserName For The System.");
 
 if (userName === "Admin") {
-    alert("Welcome Admin");
+    document.getElementById("userName").innerHTML = `Hi, ${userName}`;
+    password = prompt("Please Enter Your Password For The System"); 
+
+    if (password === "TheMaster") {
+        document.getElementById("password").innerHTML = `Hi, ${userName}, password is correct!`;
+        document.getElementById("welcome-message").innerHTML = "Welcome to the top secret mainframe that contains all the top secret stuff.";
+        alert("Welcome Admin, System Access Granted!");
+    } else if (password === "" || password === null) {
+        alert("Login Cancelled");
+    } else {
+        alert("Password Not Recognised");
+    }
  
     
-
 
 } else if (userName === '' || userName === null) {
     alert("Login Cancelled.");
